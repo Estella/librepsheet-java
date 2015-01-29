a4babc7eb9fb8c42c7fc6351d91958fa09ce0aff
Aaron Bedra and Brian Cameron <aaron@aaronbedra.com>
2015-01-29 09:47:24 -0600

    Refactor Actor class. Closes #1.
    The Actor class had too much duplication. This change breaks the
    standard keyspaces for marked, blacklisted, and whitelisted lookups. The
    new keyspace will be
    
    ```
    <entry>:repsheet:<type>:blacklisted
    <entry>:repsheet:<type>:whitelisted
    <entry>:repsheet:<type>:marked
    ```
    
    This will be reflected in the C version of librepsheet going forward.

--- src/main/java/com/repsheet/librepsheet/Actor.java
+++ src/main/java/com/repsheet/librepsheet/Actor.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Set;
 
 public class Actor {
-    public enum Type { IP, USER, CIDR };
+    public enum Type { IP, USER }
     public enum Status { MARKED, WHITELISTED, BLACKLISTED, OK }
 
     private final Type type;
@@ -34,20 +34,20 @@ public class Actor {
     public static Actor lookup(Connection connection, Type type, String value) {
         Actor actor = new Actor(type, value);
 
-        isWhitelisted(connection, actor);
+        query(connection, actor, Status.WHITELISTED);
         if (actor.reason != null) {
             actor.status = Status.WHITELISTED;
             return actor;
         }
 
-        isBlacklisted(connection, actor);
+        query(connection, actor, Status.BLACKLISTED);
         if (actor.reason != null) {
             actor.status = Status.BLACKLISTED;
             return actor;
 
         }
 
-        isMarked(connection, actor);
+        query(connection, actor, Status.MARKED);
         if (actor.reason != null) {
             actor.status = Status.MARKED;
             return actor;
@@ -58,16 +58,16 @@ public class Actor {
         return actor;
     }
 
-    private static void isWhitelisted(Connection connection, Actor actor) {
-        switch(actor.type) {
+    private static void query(Connection connection, Actor actor, Status status) {
+        switch (actor.type) {
             case IP:
                 try (Jedis jedis = connection.getPool().getResource()) {
-                    String reply = jedis.get(actor.value + ":repsheet:ip:whitelist");
+                    String reply = jedis.get(actor.value + ":repsheet:ip:" + keyspaceFromStatus(status));
                     if (reply != null) {
                         actor.reason = reply;
                     }
 
-                    Set<String> blocks = jedis.keys("*:repsheet:cidr:whitelist");
+                    Set<String> blocks = jedis.keys("*:repsheet:cidr:" + keyspaceFromStatus(status));
                     for (String s : blocks) {
                         try {
                             String[] parts = s.split(":");
@@ -76,7 +76,7 @@ public class Actor {
                             InetAddress address = InetAddress.getByName(actor.value);
                             if (cidr.contains(address)) {
                                 actor.reason = jedis.get(s);
-                                actor.status = Status.WHITELISTED;
+                                actor.status = status;
                                 return;
                             }
                         } catch (UnknownHostException e) {
@@ -86,7 +86,7 @@ public class Actor {
                 }
             case USER:
                 try (Jedis jedis = connection.getPool().getResource()) {
-                    String reply = jedis.get(actor.value + ":repsheet:users:whitelist");
+                    String reply = jedis.get(actor.value + ":repsheet:users:" + keyspaceFromStatus(status));
                     if (reply != null) {
                         actor.reason = reply;
                     }
@@ -94,75 +94,16 @@ public class Actor {
         }
     }
 
-    private static void isBlacklisted(Connection connection, Actor actor) {
-        switch (actor.type) {
-            case IP:
-                try (Jedis jedis = connection.getPool().getResource()) {
-                    String reply = jedis.get(actor.value + ":repsheet:ip:blacklist");
-                    if (reply != null) {
-                        actor.reason = reply;
-                    }
-
-                    Set<String> blocks = jedis.keys("*:repsheet:cidr:blacklist");
-                    for (String s : blocks) {
-                        try {
-                            String[] parts = s.split(":");
-                            String block = StringUtils.join(Arrays.asList(parts).subList(0, parts.length - 3), ":");
-                            CIDR cidr = CIDR.newCIDR(block);
-                            InetAddress address = InetAddress.getByName(actor.value);
-                            if (cidr.contains(address)) {
-                                actor.reason = jedis.get(s);
-                                actor.status = Status.BLACKLISTED;
-                                return;
-                            }
-                        } catch (UnknownHostException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            case USER:
-                try (Jedis jedis = connection.getPool().getResource()) {
-                    String reply = jedis.get(actor.value + ":repsheet:users:blacklist");
-                    if (reply != null) {
-                        actor.reason = reply;
-                    }
-                }
-        }
-    }
-
-    private static void isMarked(Connection connection, Actor actor) {
-        switch (actor.type) {
-            case IP:
-                try (Jedis jedis = connection.getPool().getResource()) {
-                    String reply = jedis.get(actor.value + ":repsheet:ip");
-                    if (reply != null) {
-                        actor.reason = reply;
-                    }
-
-                    Set<String> blocks = jedis.keys("*:repsheet:cidr");
-                    for (String s : blocks) {
-                        try {
-                            String[] parts = s.split(":");
-                            String block = StringUtils.join(Arrays.asList(parts).subList(0, parts.length - 2), ":");
-                            CIDR cidr = CIDR.newCIDR(block);
-                            InetAddress address = InetAddress.getByName(actor.value);
-                            if (cidr.contains(address)) {
-                                actor.reason = jedis.get(s);
-                                actor.status = Status.MARKED;
-                                return;
-                            }
-                        } catch (UnknownHostException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            case USER:
-                try (Jedis jedis = connection.getPool().getResource()) {
-                    String reply = jedis.get(actor.value + ":repsheet:users");
-                    if (reply != null) {
-                        actor.reason = reply;
-                    }
-                }
+    private static String keyspaceFromStatus(Status status) {
+        switch (status) {
+            case WHITELISTED:
+                return "whitelisted";
+            case BLACKLISTED:
+                return "blacklisted";
+            case MARKED:
+                return "marked";
+            default:
+                return null;
         }
     }
 }
--- src/test/java/com/repsheet/librepsheet/ActorTest.java
+++ src/test/java/com/repsheet/librepsheet/ActorTest.java
@@ -19,8 +19,8 @@ public class ActorTest {
     @Test
     public void testStatusReturnsWhitelistedWhenAlsoBlacklisted() {
         try (Jedis jedis = connection.getPool().getResource()) {
-            jedis.set("1.1.1.1:repsheet:ip:whitelist", "test");
-            jedis.set("1.1.1.1:repsheet:ip:blacklist", "test");
+            jedis.set("1.1.1.1:repsheet:ip:whitelisted", "test");
+            jedis.set("1.1.1.1:repsheet:ip:blacklisted", "test");
         }
         Actor actor = Actor.lookup(connection, Actor.Type.IP, "1.1.1.1");
         assertEquals(Actor.Status.WHITELISTED, actor.getStatus());
@@ -30,7 +30,7 @@ public class ActorTest {
     @Test
     public void testStatusReturnsBlacklistedWhenBlacklisted() {
         try (Jedis jedis = connection.getPool().getResource()) {
-            jedis.set("1.1.1.1:repsheet:ip:blacklist", "test");
+            jedis.set("1.1.1.1:repsheet:ip:blacklisted", "test");
         }
         Actor actor = Actor.lookup(connection, Actor.Type.IP, "1.1.1.1");
         assertEquals(Actor.Status.BLACKLISTED, actor.getStatus());
@@ -40,7 +40,7 @@ public class ActorTest {
     @Test
     public void testStatusReturnsMarkedWhenMarked() {
         try (Jedis jedis = connection.getPool().getResource()) {
-            jedis.set("1.1.1.1:repsheet:ip", "test");
+            jedis.set("1.1.1.1:repsheet:ip:marked", "test");
         }
         Actor actor = Actor.lookup(connection, Actor.Type.IP, "1.1.1.1");
         assertEquals(Actor.Status.MARKED, actor.getStatus());
@@ -56,7 +56,7 @@ public class ActorTest {
     @Test
     public void testStatusReturnsWhitelistedWhenIPv4InCIDRBlock() {
         try (Jedis jedis = connection.getPool().getResource()) {
-            jedis.set("10.0.1.0/24:repsheet:cidr:whitelist", "test");
+            jedis.set("10.0.1.0/24:repsheet:cidr:whitelisted", "test");
         }
         Actor actor = Actor.lookup(connection, Actor.Type.IP, "10.0.1.15");
         assertEquals(Actor.Status.WHITELISTED, actor.getStatus());
@@ -65,7 +65,7 @@ public class ActorTest {
     @Test
     public void testStatusReturnsBlacklistedWhenIPv6InCIDRBlock() {
         try (Jedis jedis = connection.getPool().getResource()) {
-            jedis.set("1fff:0:0a88:85a3:0:0:ac1f:8001/24:repsheet:cidr:blacklist", "test");
+            jedis.set("1fff:0:0a88:85a3:0:0:ac1f:8001/24:repsheet:cidr:blacklisted", "test");
         }
         Actor actor = Actor.lookup(connection, Actor.Type.IP, "1fff:0:0a88:85a3:0:0:ac1f:8002");
         assertEquals(Actor.Status.BLACKLISTED, actor.getStatus());
@@ -74,7 +74,7 @@ public class ActorTest {
     @Test
     public void testStatusReturnsMarkedWhenIPv4InCIDRBlock() {
         try (Jedis jedis = connection.getPool().getResource()) {
-            jedis.set("10.0.1.0/24:repsheet:cidr", "test");
+            jedis.set("10.0.1.0/24:repsheet:cidr:marked", "test");
         }
         Actor actor = Actor.lookup(connection, Actor.Type.IP, "10.0.1.15");
         assertEquals(Actor.Status.MARKED, actor.getStatus());
