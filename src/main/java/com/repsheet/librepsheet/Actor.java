4eb50b5110f1f29e8bdaaa44e0484f487002bb3b
Aaron Bedra <aaron@aaronbedra.com>
2015-10-05 15:22:27 -0500

    Fix unused import

--- src/main/java/com/repsheet/librepsheet/Actor.java
+++ src/main/java/com/repsheet/librepsheet/Actor.java
@@ -1,7 +1,6 @@
 package com.repsheet.librepsheet;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 import redis.clients.util.Pool;
 
